ca3fda24d2752ed23e23f158557b4521517d1fbb
Jônatas Souza Soares <56240097+jonatasJS@users.noreply.github.com>
2022-02-13 02:06:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ app.use(express.static('pages'));
 app.get('/v1/cep/:cep', async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   const cep = req.params.cep;
-  const resData = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+  const resData = await axios.get(`https://viacep.com.br/ws/${cep}/json/`); 
+  
   return await res.json(resData.data);
 });
 
